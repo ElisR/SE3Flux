2eab69b2c554e9f9ddd922706fb0b7f04728bbd9
Elis Roberts <elisiforroberts@gmail.com>
2023-01-13 11:10:48 +0000

    Change ParallelPassenger to act on singletons Adding this functionality to existing structure.

--- alt_parallel.jl
+++ alt_parallel.jl
@@ -3,46 +3,48 @@ using ChainRulesCore
 struct ParallelPassenger{F, T}
     connection::F
     layers::T
+
+    singleton::Bool
 end
 
-# allow ParallelPassenger(op, m1, m2, ...) as a constructor
-#ParallelPassenger(connection, layers...) = ParallelPassenger(connection, layers)
-#ParallelPassenger(connection, (layer,)) = layer
-#function ParallelPassenger(connection, layers::Tuple)
-#    if isempty(layers)
-#        return Passenger(identity)
-#    else
-#        return ParallelPassenger(connection, layers)
-#    end
-#end
+function ParallelPassenger(connection, layers; singleton=false)
+    ParallelPassenger(connection, layers, singleton)
+end
+
 ParallelPassenger(connection) = Passenger(identity)
 
-Flux.@functor ParallelPassenger
-
-# TODO Fix this part. (May already be fixed)
-# This is currently not working, so will rewrite to be more like original
-#=
-(pp::ParallelPassenger)((r, y)::Tuple) = pp(r, y)
-(pp::ParallelPassenger)(r, xs::Tuple) = pp.connection(map((f, x) -> f(r, x), Tuple(pp.layers), xs)...)
-(pp::ParallelPassenger)(r, (x,)) = pp.connection(map(f -> f(r, x), Tuple(pp.layers))...) # This
-
-(pp::ParallelPassenger)(r, xs...) = pp(r, xs)
-# New vector implementation. Unexpected behaviour, but what I need in E3ConvLayer
-(pp::ParallelPassenger)(r, xs::Vector) = pp.connection(map(f -> f(r, xs...), Tuple(pp.layers))...)
-=#
+Flux.@functor ParallelPassenger (layers,)
 
 (pp::ParallelPassenger)(rx::Tuple) = pp(rx...)
-(pp::ParallelPassenger)(r, xs::Tuple) = pp(r, xs...)
 
-(pp::ParallelPassenger)(r, x) = pp.connection(map(f -> f(r, x), pp.layers)...)
+function (pp::ParallelPassenger)(r, xs::Tuple)
+    if pp.singleton
+        pp.connection(map(f -> f(r, xs), pp.layers)...)
+    else
+        _parallel_check(pp.layers, xs)
+        pp.connection(map((f, x) -> f(r, x), pp.layers, xs)...)
+    end
+end
+
+#(pp::ParallelPassenger)(r, x) = pp.connection(map(f -> f(r, x), pp.layers)...)
+# TODO Replace this with slurped version
 function (pp::ParallelPassenger)(r, xs...)
-    _parallel_check(pp.layers, xs)
-    pp.connection(map((f, x) -> f(r, x), pp.layers, xs)...)
+    if pp.singleton
+        pp.connection(map(f -> f(r, xs), pp.layers)...)
+    else
+        _parallel_check(pp.layers, xs)
+        pp.connection(map((f, x) -> f(r, x), pp.layers, xs)...)
+    end
 end
 # Just added this / might be wrong
 function (pp::ParallelPassenger)(r, xs::Vector)
-    _parallel_check(pp.layers, xs)
-    pp.connection(map((f, x) -> f(r, x), pp.layers, Tuple(xs))...)
+    if pp.singleton
+        pp.connection(map(f -> f(r, xs), pp.layers)...)
+    else
+        _parallel_check(pp.layers, xs)
+        pp.connection(map((f, x) -> f(r, x), pp.layers, xs)...)
+    end
+    #pp.connection(map(f -> f(r, Tuple(xs)), pp.layers)...)
 end
 
 # TODO This is not currently used
@@ -50,7 +52,7 @@ function _parallel_check(layers, xs)
     nl = length(layers)
     nx = length(xs) 
     if (nl != nx)
-      throw(ArgumentError("Parallel with $nl sub-layers can take one input or $nl inputs, but got $nx inputs"))
+        throw(ArgumentError("Parallel with $nl sub-layers can take one input or $nl inputs, but got $nx inputs"))
     end
   end
 ChainRulesCore.@non_differentiable _parallel_check(nl, nx)
